3b02bcf68d283126c6fc0a5869b97ef930d773b0
zuzihe <dya_yaaa@naver.com>
2024-11-20 00:49:49 +0900

    서비스 소개

--- src/pages/Introduction.js
+++ src/pages/Introduction.js
@@ -1,8 +1,14 @@
 // src/pages/Introduction.js
 import React from 'react';
+import '../style/Introduction.css'; // 스타일을 별도의 CSS 파일에서 관리
+
 
 const Introduction = () => {
-  return <div>서비스소개 화면</div>;
+  return (
+    <div className="container">
+    <img src="/introduction.png" alt="서비스 소개 이미지" className="intro-image" />
+  </div>
+  )
 };
 
 export default Introduction;
