6953be18e0b834b41fdbe9c948cae57b7c59ee66
Joohyun Lee <2271183@hansung.ac.kr>
2024-12-03 04:46:24 +0900

    animation

--- src/pages/Introduction.js
+++ src/pages/Introduction.js
@@ -1,14 +1,42 @@
 // src/pages/Introduction.js
-import React from 'react';
-import '../style/Introduction.css'; // 스타일을 별도의 CSS 파일에서 관리
-
+import React, { useEffect, useRef, useState } from "react";
+import '../style/Introduction.css';
 
 const Introduction = () => {
+  const photoRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+        }
+      },
+      { threshold: 0.1 } // 10% 보이면 트리거
+    );
+
+    if (photoRef.current) {
+      observer.observe(photoRef.current);
+    }
+
+    return () => {
+      if (photoRef.current) {
+        observer.unobserve(photoRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <div className="container">
-      <img src="/introduction.png" alt="서비스 소개 이미지" className="intro-image" />
+    <div className="scroll-container">
+      <div
+        className={`photo ${isVisible ? "visible" : ""}`}
+        ref={photoRef}
+      >
+        <img src="/introduction.png" alt="Photo" />
+      </div>
     </div>
-  )
+  );
 };
 
 export default Introduction;
